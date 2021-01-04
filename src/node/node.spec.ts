d8663929f4b4d9bda14ca84688af8aa7ca2d9442
HamedPour <hamed_k.pour@hotmail.com>
2021-01-04 17:42:26 +0000

    node unit tests

---
+++ src/node/node.spec.ts
@@ -0,0 +1,25 @@
+import { NodeDrawComponent } from './components'
+import { Node } from './node'
+
+
+describe('>>> Node', () => {
+  let node: Node
+
+  beforeEach(()=> {
+    node = new Node()
+  })
+
+  it('should awake and update all components', ()=> {
+    const spyDrawCompAwake = jest.spyOn(NodeDrawComponent.prototype, 'Awake')
+    const spyDrawCompUpdate = jest.spyOn(NodeDrawComponent.prototype, 'Update')
+
+    expect(spyDrawCompAwake).not.toBeCalled()
+    expect(spyDrawCompUpdate).not.toBeCalled()
+
+    node.Awake()
+    expect(spyDrawCompAwake).toBeCalled()
+
+    node.Update(0)
+    expect(spyDrawCompUpdate).toBeCalled()
+  })
+})
